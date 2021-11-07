c4a0855b4eb8bf3bebc5064ccec592276df29976
Yaroslav Frolov <yaroslav.frolov@altenar.com>
2021-11-07 18:06:19 +0300

    Week 2 homework

---
+++ aTES.Auth/ProviderConfig.cs
@@ -0,0 +1,68 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace aTES.Auth
+{
+    /// <summary>
+    /// Hardcoded popug structure for identity server
+    /// </summary>
+    public static class ProviderConfig
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource("publickey", new [] {"publickey"}),
+                new IdentityResource("roles", new[] { "role" })
+            };
+
+
+        public static IEnumerable<ApiResource> Apis =>
+            new List<ApiResource>
+            {  
+                //no apis to protect for now, only some blazor web apps
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "aTES.Auth",
+                    ClientName="aTES Authentification Service",
+                    ClientSecrets = { new Secret("aTES.Auth".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = false,
+                    RequirePkce = true,
+
+                    RedirectUris = { "http://localhost:27627/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:27627/signout-callback-oidc" },
+
+                    AllowedScopes = { "openid", "profile", "email", "roles", "publickey" },
+
+                    AllowOfflineAccess = true
+                },
+                new Client
+                {
+                    ClientId = "aTES.Tasks",
+                    ClientName="aTES Task Service",
+                    ClientSecrets = { new Secret("aTES.Tasks".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = false,
+                    RequirePkce = true,
+
+                    RedirectUris = { "http://localhost:30478/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:30478/signout-callback-oidc" },
+
+                    AllowedScopes = { "openid", "profile", "email", "roles", "publickey" },
+
+                    AllowOfflineAccess = true
+                }
+            };   
+    }
+}
